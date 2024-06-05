c757df57bea0faab103e4242f47fc8319718abf4
Abdul Moiz <mymail1461@gmail.com>
2024-06-05 19:04:23 +0500

    Controller complete

--- Entity_Framework/Controllers/StudentsController.cs
+++ Entity_Framework/Controllers/StudentsController.cs
@@ -34,7 +34,59 @@ namespace Entity_Framework.Controllers
             return student;
         }
 
-        // [HttpPost]
+        [HttpPost]
+        public async Task<ActionResult<Student>> PostStudent(Student student)
+        {
+            _context.Students.Add(student);
+            await _context.SaveChangesAsync();
 
+            return CreatedAtAction(nameof(Student), new {id = student.StudentId}, student);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutStudent(int id, Student student)
+        {
+            if (id != student.StudentId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(student).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!StudentExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return NoContent();
+        }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteStudent(int id)
+        {
+            var student = await _context.Students.FindAsync(id);
+            if (student == null) 
+            {
+                return NotFound();
+            }
+            _context.Students.Remove(student);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool StudentExists(int id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
