bf577f2262461d079cb194fcab6a7c2290762c4d
Abdul Moiz <mymail1461@gmail.com>
2024-06-05 18:53:08 +0500

    HttpGet and HttpGet by Id endpoint has been added

---
+++ Entity_Framework/Controllers/StudentsController.cs
@@ -0,0 +1,40 @@
+﻿using Entity_Framework.Data;
+using Entity_Framework.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Entity_Framework.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        private readonly SchoolDbContext _context;
+
+        public StudentsController(SchoolDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Student>>> GetStudents()
+        {
+            return await _context.Students.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Student>> GetStudent(int id)
+        {
+            var student = await _context.Students.FindAsync(id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+            return student;
+        }
+
+        // [HttpPost]
+
+    }
+}
