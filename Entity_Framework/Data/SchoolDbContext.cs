7334f855dc30c8debffa97db557780a98964d821
Abdul Moiz <mymail1461@gmail.com>
2024-06-05 18:42:41 +0500

    Database populated with Entities.

--- Entity_Framework/Data/SchoolDbContext.cs
+++ Entity_Framework/Data/SchoolDbContext.cs
@@ -1,7 +1,24 @@
-﻿namespace Entity_Framework.Data
-{
-    public class SchoolDbContext
-    {
+﻿using Entity_Framework.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
+namespace Entity_Framework.Data
+{
+    public class SchoolDbContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Department> Departments { get; set; } 
+        public DbSet<Instructor> Instructors { get; set; }
+
+        public SchoolDbContext(DbContextOptions<SchoolDbContext> options) : base(options) { }
+
+        
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Department>().Property(d => d.Budget).HasColumnType("decimal(18, 2)");
+        }
     }
 }
--- Entity_Framework/Program.cs
+++ Entity_Framework/Program.cs
@@ -1,9 +1,12 @@
+using Entity_Framework.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddDbContext<SchoolDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
